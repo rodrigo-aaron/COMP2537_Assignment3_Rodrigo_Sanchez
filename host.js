e7d4221300eb3b79515b0c46a633d3030950a08d
Rodrigo Sanchez <rodrigo.aaron.sa@gmail.com>
2025-05-19 22:47:36 -0700

    quick fix at index.js

--- host.js
+++ host.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the current directory
+
 app.use(express.static(path.join(__dirname)));
 
-// Serve index.html at the root route
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Running at http://localhost:${port}`);
 });
